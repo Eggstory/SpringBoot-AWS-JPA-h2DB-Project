039fefd5aea4b58f4077d03b5fab7e81161465cc
khm9963 <khm9963@naver.com>
2022-03-28 19:58:24 +0900

    DB에서 값 받아와 목록에 띄우기 (성공)

--- src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -1,11 +1,15 @@
 package com.jojoldu.book.springboot.service.posts;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -15,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Service
 public class PostsService {
+	
 	private final PostsRepository postsRepository;
 	
 	@Transactional
@@ -37,6 +42,14 @@ public class PostsService {
 		new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 		
 		return new PostsResponseDto(entity);
+	}
+	
+	@Transactional(readOnly = true)
+	public List<PostsListResponseDto> findAllDesc() {
+		return postsRepository.findAllDesc().stream()
+				.map(PostsListResponseDto::new)
+				.collect(Collectors.toList());
+		
 	}
 
 }
--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,7 +1,14 @@
 package com.jojoldu.book.springboot.web;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.jojoldu.book.springboot.domain.posts.Posts;
+import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.service.posts.PostsService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,14 +16,20 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class IndexController {
 	
+	private final PostsService postsService;
+	
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("posts", postsService.findAllDesc());
 		return "index";
 	}
+	
 	
 	@GetMapping("/posts/save")
 	public String postsSave() {
 		return "posts-save";
 	}
+
+	
 
 }
